97f7af7594fb3eb1cb5ae57b8401a552024552a5
Pavel <pasaha@list.ru>
2016-02-24 01:59:11 +0300

    gallery

--- www/template/menu.php
+++ www/template/menu.php
@@ -1,11 +1,6 @@
 <?php
-
+// ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
 $menu_items = json_decode('[
-	{
-		"title": "Works",
-		"id":  "work",
-		"tpl":   "template/index/work.php"
-	 },
 	{
 		"title": "Home",
 		"id":  "home",
@@ -17,10 +12,42 @@ $menu_items = json_decode('[
 		"tpl":   "template/index/about.php"
 	 },
 	{
+		"title": "Works",
+		"id":  "work",
+		"tpl":   "template/index/work.php"
+	 },
+	{
 		"title": "Contact",
 		"id":  "contact",
 		"tpl":   "template/index/contact.php"
 	 }
 ]',true);
 
+
+// ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+// ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+
+$portfolio_items = json_decode('[
+	{
+		"title":   "Fashion",
+		"preview": "/images/portfolio/500x400-fashion.jpg",
+		"url":     "#!index.php?action=portfolio-gallery&alias=gallery-fashion"
+	 },
+	{
+		"title":   "Portrait",
+		"preview": "/images/portfolio/500x400-portrait.jpg",
+		"url":     "#!index.php?action=portfolio-gallery&alias=gallery-portrait"
+	 },
+	{
+		"title":   "Shadow",
+		"preview": "/images/portfolio/500x400-shadow.jpg",
+		"url":     "#!index.php?action=portfolio-gallery&alias=gallery-shadow"
+	 },
+	{
+		"title":   "Street",
+		"preview": "/images/portfolio/500x400-street.jpg",
+		"url":     "#!index.php?action=portfolio-gallery&alias=gallery-street"
+	 }
+]',true);
+// ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
 ?>
