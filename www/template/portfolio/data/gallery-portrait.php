37a1106532352feb7ac866d314f27c551205194b
Pavel <pasaha@list.ru>
2016-04-11 01:18:14 +0300

    medowaya

--- www/template/portfolio/data/gallery-portrait.php
+++ www/template/portfolio/data/gallery-portrait.php
@@ -3,18 +3,18 @@
 $gallery_data = json_decode('{
 	"title":   "Portrait",
 	"items": [
-		{"preview": "/images/portfolio/portrait/preview/01.jpg", "image": "/images/portfolio/portrait/01.jpg"},
+		{"preview": "/images/portfolio/portrait/preview/23.jpg", "image": "/images/portfolio/portrait/23.jpg"},
+		{"preview": "/images/portfolio/portrait/preview/26.jpg", "image": "/images/portfolio/portrait/26.jpg"},
 		{"preview": "/images/portfolio/portrait/preview/02.jpg", "image": "/images/portfolio/portrait/02.jpg"},
+		{"preview": "/images/portfolio/portrait/preview/021.jpg", "image": "/images/portfolio/portrait/021.jpg"},
 		{"preview": "/images/portfolio/portrait/preview/03.jpg", "image": "/images/portfolio/portrait/03.jpg"},
 		{"preview": "/images/portfolio/portrait/preview/04.jpg", "image": "/images/portfolio/portrait/04.jpg"},
 		{"preview": "/images/portfolio/portrait/preview/05.jpg", "image": "/images/portfolio/portrait/05.jpg"},
-		{"preview": "/images/portfolio/portrait/preview/06.jpg", "image": "/images/portfolio/portrait/06.jpg"},
-		{"preview": "/images/portfolio/portrait/preview/07.jpg", "image": "/images/portfolio/portrait/07.jpg"},
-		{"preview": "/images/portfolio/portrait/preview/08.jpg", "image": "/images/portfolio/portrait/08.jpg"},
 		{"preview": "/images/portfolio/portrait/preview/09.jpg", "image": "/images/portfolio/portrait/09.jpg"},
 		{"preview": "/images/portfolio/portrait/preview/10.jpg", "image": "/images/portfolio/portrait/10.jpg"},
 		{"preview": "/images/portfolio/portrait/preview/11.jpg", "image": "/images/portfolio/portrait/11.jpg"},
 		{"preview": "/images/portfolio/portrait/preview/12.jpg", "image": "/images/portfolio/portrait/12.jpg"},
+		{"preview": "/images/portfolio/portrait/preview/121.jpg", "image": "/images/portfolio/portrait/121.jpg"},
 		{"preview": "/images/portfolio/portrait/preview/13.jpg", "image": "/images/portfolio/portrait/13.jpg"},
 		{"preview": "/images/portfolio/portrait/preview/14.jpg", "image": "/images/portfolio/portrait/14.jpg"},
 		{"preview": "/images/portfolio/portrait/preview/15.jpg", "image": "/images/portfolio/portrait/15.jpg"},
@@ -25,10 +25,9 @@ $gallery_data = json_decode('{
 		{"preview": "/images/portfolio/portrait/preview/20.jpg", "image": "/images/portfolio/portrait/20.jpg"},
 		{"preview": "/images/portfolio/portrait/preview/21.jpg", "image": "/images/portfolio/portrait/21.jpg"},
 		{"preview": "/images/portfolio/portrait/preview/22.jpg", "image": "/images/portfolio/portrait/22.jpg"},
-		{"preview": "/images/portfolio/portrait/preview/23.jpg", "image": "/images/portfolio/portrait/23.jpg"},
 		{"preview": "/images/portfolio/portrait/preview/24.jpg", "image": "/images/portfolio/portrait/24.jpg"},
-		{"preview": "/images/portfolio/portrait/preview/25.jpg", "image": "/images/portfolio/portrait/25.jpg"},
-		{"preview": "/images/portfolio/portrait/preview/26.jpg", "image": "/images/portfolio/portrait/26.jpg"}
+		{"preview": "/images/portfolio/portrait/preview/251.jpg", "image": "/images/portfolio/portrait/251.jpg"},
+		{"preview": "/images/portfolio/portrait/preview/01.jpg", "image": "/images/portfolio/portrait/01.jpg"}
 	]
 }
 ',true);
