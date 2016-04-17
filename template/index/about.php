3e158616bcfdce2fcc424aae9e996d84389fea40
108344 <108344@linwebng02.(none)>
2016-04-17 18:15:51 +0300

    about

--- template/index/about.php
+++ template/index/about.php
@@ -19,17 +19,23 @@
 								<strong>Portrait and fashion photographer, art critic, blogger</strong>
 							</p>
 							<p>
-								<span class="highlight">Born in&nbsp;1984</span> in&nbsp;St. Petersburg. She graduated from the Faculty of&nbsp;&laquo;History of&nbsp;World Culture&raquo; Saint-Petersburg State University of&nbsp;Culture and Arts with a&nbsp;degree in&nbsp;art, the world art of&nbsp;fashion. She studied with leading teachers and professionals of&nbsp;fashion and costume history, the history of&nbsp;fashion photography and journalism. Received additional education in&nbsp;the New School Photo College, St. Petersburg, courses Professional photographer, Art retouching, Street Fashion.
+								<span class="highlight">Born in&nbsp;1984</span> in&nbsp;St. Petersburg,
+                she graduated from the Faculty of&nbsp;&laquo;History of&nbsp;World Culture&raquo; Saint-Petersburg State University of&nbsp;Culture and Arts 
+                with a&nbsp;degree in&nbsp;art, the world art of&nbsp;fashion. 
+                She studied with leading teachers and professionals of&nbsp;fashion and costume history, the history of&nbsp;fashion photography and journalism. 
+                Received additional education in&nbsp;the New School Photo College, St. Petersburg, courses Professional photography, Art retouching, and Street Fashion.
 							</p>
 							<p>
-								<span class="highlight">Since 2011</span> she has been working as&nbsp;a&nbsp;photographer&nbsp;&mdash; a&nbsp;freelancer.
-								She collaborated with fashion designers, stylists, make-up artists, model agencies.
+								<span class="highlight">Since 2011</span> she has been working as&nbsp;a&nbsp;photographer&nbsp;&mdash; a&nbsp;freelance photographer.
+								She has collaborated with fashion designers, stylists, make-up artists, model agencies.
 							</p>
 							<p>
-								<span class="highlight">In&nbsp;2014</span> she began to&nbsp;work on&nbsp;the author&rsquo;s art project &laquo;The Shadow&raquo;, later published in&nbsp;the magazine &laquo;Your story&raquo;.
+								<span class="highlight">In&nbsp;2014</span> she began to&nbsp;work on&nbsp;the author&rsquo;s art project &laquo;The Shadow&raquo;, 
+                later published in&nbsp;the magazine &laquo;Your story&raquo;.
 							</p>
 							<p>
-								<span class="highlight">Since 2015</span> she lives in&nbsp;NY, USA, where she continued career as&nbsp;a&nbsp;photographer, writes articles, books, create art projects, realize into reality the most daring dreams.
+								<span class="highlight">Since 2015</span> she resides in&nbsp;NY, USA, where she continues her career as&nbsp;a&nbsp;photographer, 
+                writer, creates art projects, bringing to fruition her most daring dreams.
 							</p>
 						</div>
 					</div>
